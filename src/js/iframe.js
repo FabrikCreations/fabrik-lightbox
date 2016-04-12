928e0f6d531fc180e578a5de93ccf922fd4d524e
phil118 <p.jenkins118@gmail.com>
2016-04-12 18:14:33 +0100

    no scrolling and moved over to fabrik embed service

--- src/js/iframe.js
+++ src/js/iframe.js
@@ -22,7 +22,7 @@
     options: {
       markup: '<div class="mfp-iframe-scaler">' +
                 '<div class="mfp-close"></div>' +
-                '<iframe class="mfp-iframe" src="//about:blank" frameborder="0" allowfullscreen></iframe>' +
+                '<iframe class="mfp-iframe" src="//about:blank" frameborder="0" allowfullscreen scrolling="no"></iframe>' +
                 '<div class="mfp-title"></div>' +
               '</div>',
       srcAction: 'iframe_src',
@@ -51,32 +51,26 @@
       getIframe: function (item, template) {
         var embedSrc = item.src;
         var iframeSt = mfp.st.iframe;
+        
+        fabrik.embedService.getEmbed(embedSrc).done(function (data) {
+          embedSrc = $(data.html);
 
-        $.ajax({
-          url: "/media/embed?url=" + embedSrc,
-          success: function (data, textStatus, jqXHR) {
-            embedSrc = $(data);
+          var dataObj = {};
 
-            var dataObj = {};
-
-            if (iframeSt.srcAction) {
-              dataObj[iframeSt.srcAction] = embedSrc[0].src;
-            }
-
-            var src = iframeSt.titleSrc;
-
-            if (item.el) {
-              dataObj.title = item.el.attr(src);
-            }
-
-            mfp._parseMarkup(template, dataObj, item);
-
-            mfp.updateStatus('ready');
-
-          },
-          error: function () {
-
+          if (iframeSt.srcAction) {
+            dataObj[iframeSt.srcAction] = embedSrc[0].src;
           }
+
+          var src = iframeSt.titleSrc;
+
+          if (item.el) {
+            dataObj.title = item.el.attr(src);
+          }
+
+          mfp._parseMarkup(template, dataObj, item);
+
+          mfp.updateStatus('ready');
+
         });
 
         return template;
--- website/dist/jquery.magnific-popup.js
+++ website/dist/jquery.magnific-popup.js
@@ -1569,7 +1569,7 @@ $.magnificPopup.registerModule('zoom', {
     options: {
       markup: '<div class="mfp-iframe-scaler">' +
                 '<div class="mfp-close"></div>' +
-                '<iframe class="mfp-iframe" src="//about:blank" frameborder="0" allowfullscreen></iframe>' +
+                '<iframe class="mfp-iframe" src="//about:blank" frameborder="0" allowfullscreen scrolling="no"></iframe>' +
                 '<div class="mfp-title"></div>' +
               '</div>',
       srcAction: 'iframe_src',
@@ -1598,32 +1598,26 @@ $.magnificPopup.registerModule('zoom', {
       getIframe: function (item, template) {
         var embedSrc = item.src;
         var iframeSt = mfp.st.iframe;
+        
+        fabrik.embedService.getEmbed(embedSrc).done(function (data) {
+          embedSrc = $(data.html);
 
-        $.ajax({
-          url: "/media/embed?url=" + embedSrc,
-          success: function (data, textStatus, jqXHR) {
-            embedSrc = $(data);
+          var dataObj = {};
 
-            var dataObj = {};
-
-            if (iframeSt.srcAction) {
-              dataObj[iframeSt.srcAction] = embedSrc[0].src;
-            }
-
-            var src = iframeSt.titleSrc;
-
-            if (item.el) {
-              dataObj.title = item.el.attr(src);
-            }
-
-            mfp._parseMarkup(template, dataObj, item);
-
-            mfp.updateStatus('ready');
-
-          },
-          error: function () {
-
+          if (iframeSt.srcAction) {
+            dataObj[iframeSt.srcAction] = embedSrc[0].src;
           }
+
+          var src = iframeSt.titleSrc;
+
+          if (item.el) {
+            dataObj.title = item.el.attr(src);
+          }
+
+          mfp._parseMarkup(template, dataObj, item);
+
+          mfp.updateStatus('ready');
+
         });
 
         return template;
