d98fcd9f21a3663a2c64f0ddfa93c7c92fb06dc4
phil118 <p.jenkins118@gmail.com>
2016-04-13 14:44:04 +0100

    added autoplay to the iframe embed service

--- _site/dist/jquery.magnific-popup.js
+++ _site/dist/jquery.magnific-popup.js
@@ -1,4 +1,4 @@
-/*! Magnific Popup - v1.1.0 - 2016-04-12
+/*! Magnific Popup - v1.1.0 - 2016-04-13
 * http://dimsemenov.com/plugins/magnific-popup/
 * Copyright (c) 2016 Dmitry Semenov; */
 ;(function (factory) { 
