aca3746c6e7fb9ffb90cf1e6e27e72d4cc39e818
phil118 <p.jenkins118@gmail.com>
2016-05-25 17:52:16 +0100

    added fitframe wrap to the default template

--- website/dist/jquery.magnific-popup.js
+++ website/dist/jquery.magnific-popup.js
@@ -1569,7 +1569,9 @@ $.magnificPopup.registerModule('zoom', {
     options: {
       markup: '<div class="mfp-iframe-scaler">' +
                 '<div class="mfp-close"></div>' +
-                '<iframe class="mfp-iframe" src="//about:blank" frameborder="0" allowfullscreen scrolling="no"></iframe>' +
+                '<div class="mfp-fitframe fitframe-wrap">' + 
+                  '<iframe class="mfp-iframe" src="//about:blank" frameborder="0" allowfullscreen scrolling="no"></iframe>' +
+                '<div>' +
                 '<div class="mfp-title"></div>' +
               '</div>',
       srcAction: 'iframe_src',
